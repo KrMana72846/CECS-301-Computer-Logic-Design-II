4f0c1744265d2aa3900d11439d4eee6a9964a9b9
KrMana72846 <104547554+KrMana72846@users.noreply.github.com>
2022-04-27 23:34:07 -0700

    Delete System on Chip (SOC) Applications Matlab Labs directory

--- System on Chip (SOC) Applications Matlab Labs/dtft.m
+++
@@ -1,9 +0,0 @@
-function [X] = dtft(x,n,w) % Computes Discrete-time Fourier Transform
-% [X] = dtft(x,n,w)
-% X =DTFT values computed at w frequencies
-% x =finite duration sequence over n
-% n =sample position vector
-% w =frequency location vector
-X = exp(-1i.*w'*n)  * x';
-end
-
